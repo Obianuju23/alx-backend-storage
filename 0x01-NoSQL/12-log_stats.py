2495b5453022b1003d6080c8663aced00d132493
Obianuju23 <obianujunmoh@gmail.com>
2023-10-17 09:17:16 -0700

    Python script that provides some stats about Nginx logs stored in MongoDB

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Python script that provides some stats about Nginx logs stored in MongoDB"""
+from pymongo import MongoClient
+
+
+def nginx_stat_log(nginx_collection):
+    """Function that provide some stats about Nginx logs"""
+    client = MongoClient('mongodb://localhost:27017')
+    collection = client.logs.nginx
+
+    Total_docs = collection.count_docs({})
+    get = collection.count_docs({"method": "GET"})
+    post = collection.count_docs({"method": "POST"})
+    put = collection.count_docs({"method": "PUT"})
+    patch = collection.count_docs({"method": "PATCH"})
+    delete = collection.count_docs({"method": "DELETE"})
+    path = collection.count_docs({"method": "GET", "path": "/status"})
+
+    print("{:d} logs". format(total))
+    print("Methods:")
+    print("\tmethod GET: {:d}". format(get))
+    print("\tmethod POST: {:d}". format(post))
+    print("\tmethod PUT: {:d}". format(put))
+    print("\tmethod PATCH: {:d}". format(patch))
+    print("\tmethod DELETE: {:d}". format(delete))
+    print("{:d} status check". format(path))
