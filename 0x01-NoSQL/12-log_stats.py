b76ee85f86ec279f7fc8a21fd9ee684e34c380cf
Obianuju23 <obianujunmoh@gmail.com>
2023-10-17 18:51:33 -0700

    Python script that provides some stats about Nginx logs stored in MongoDB

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,34 +1,23 @@
 #!/usr/bin/env python3
-"""
-This module contains a function that provides
-some stats about Nginx logs stored in MongoDB
-"""
+
+'''A Python module tha provides stats about nginx'''
+
 from pymongo import MongoClient
 
 
-def log_statistics():
-    """a function that provides some stats about nginx logs"""
-    client = MongoClient()
-    db = client.logs
-    collection = db.nginx
+if __name__ == '__main__':
+    '''Prints the log stats in nginx collection'''
+    con = MongoClient('mongodb://localhost:27017')
+    collection = con.logs.nginx
 
-    total = collection.count_documents({})
-    get = collection.count_documents({"method": "GET"})
-    post = collection.count_documents({"method": "POST"})
-    put = collection.count_documents({"method": "PUT"})
-    patch = collection.count_documents({"method": "PATCH"})
-    delete = collection.count_documents({"method": "DELETE"})
-    path = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f'{collection.estimated_document_count()} logs')
 
-    print("{:d} logs". format(total))
-    print("Methods:")
-    print("\tmethod GET: {:d}". format(get))
-    print("\tmethod POST: {:d}". format(post))
-    print("\tmethod PUT: {:d}". format(put))
-    print("\tmethod PATCH: {:d}". format(patch))
-    print("\tmethod DELETE: {:d}". format(delete))
-    print("{:d} status check". format(path))
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    print('Methods:')
 
+    for req in methods:
+        print('\tmethods {}: {}'.format(req,
+              collection.count_documents({'method': req})))
 
-if __name__ == "__main__":
-    log_statistics()
+    print('{} status check'.format(collection.count_documents(
+          {'method': 'GET', 'path': '/status'})))
