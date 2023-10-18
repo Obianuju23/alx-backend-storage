a6367b16e83fa8c730bae4aab6c9d1c7b4251589
Obianuju23 <obianujunmoh@gmail.com>
2023-10-18 09:08:03 -0700

    Python script that provides some stats about Nginx logs stored in MongoDB

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,3 +1,29 @@
 #!/usr/bin/env python3
 """Python script that provides stats about Nginx logs stored in MongoDB"""
-import pymongo
+from pymongo import MongoClient
+
+
+def collection(db: dict) -> int:
+    """Function to retroeve logs information"""
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
+    return logs.count_documents(db)
+
+
+def main():
+    """Function that returns stats about Nginx logs stored in MongoDB"""
+
+    print(f"{collection({})} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {collection({'method': 'GET'})}")
+    print(f"\tmethod POST: {collection({'method': 'POST'})}")
+    print(f"\tmethod PUT: {collection({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {collection({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {collection({'method': 'DELETE'})}")
+    print(f"{collection({'method': 'GET', 'path': '/status'})} status check")
+
+
+if __name__ == "__main__":
+    main()
+
+
